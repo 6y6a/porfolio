3df5d0fae610f6a45e359ae0b38c102ec6149ee7
6y6a <gubinalexeymain@gmail.com>
2022-09-03 18:34:10 +0300

    feat(projects): create projects page

---
+++ src/pages/projects.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+
+const Projects: NextPage = () => {
+    return (
+        <div>
+            <h1>Projects page</h1>
+            <h2>Under construction</h2>
+        </div>
+    );
+};
+
+export default Projects;
