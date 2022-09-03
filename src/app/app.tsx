ac91a392ca087e2c6e961cacee4b2ae78c90c385
6y6a <gubinalexeymain@gmail.com>
2022-09-03 18:39:55 +0300

    feat(app): integrate layout in app

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { AppProps } from "./app.types";
+import { Layout } from "../shared";
 
 export const App: FC<AppProps> = ({ children }) => {
-    return <>{children}</>;
+    return <Layout>{children}</Layout>;
 };
