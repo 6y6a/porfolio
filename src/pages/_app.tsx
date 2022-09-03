350d77889a3c7e99fb064c52f9a7be489092fbe1
6y6a <gubinalexeymain@gmail.com>
2022-09-03 17:41:24 +0300

    feat(app): integrate app component to pages/_app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import "../app/styles/globals.css";
 import type { AppProps } from "next/app";
+import { App } from "../app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
-}
+const Application = ({ Component, pageProps, router, ...rest }: AppProps) => {
+    return (
+        <App
+            pageProps={pageProps}
+            Component={Component}
+            router={router}
+            {...rest}
+        >
+            <Component {...pageProps} />
+        </App>
+    );
+};
 
-export default MyApp;
+export default Application;
