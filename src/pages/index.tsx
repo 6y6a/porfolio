09143941136d0ab7aa533a24e7e9f1890047de7e
6y6a <gubinalexeymain@gmail.com>
2022-09-03 18:46:20 +0300

    feat(index): integrate hero on page

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { Hero } from "../features/hero";
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <Hero />
+        </>
+    );
+};
+
+export default Home;
