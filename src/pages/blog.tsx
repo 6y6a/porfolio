5835622d4cd64ebbb116db627743ef3f87a05788
6y6a <gubinalexeymain@gmail.com>
2022-09-03 18:34:25 +0300

    feat(blog): create blog page

---
+++ src/pages/blog.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+
+const Blog: NextPage = () => {
+    return (
+        <div>
+            <h1>Blog page</h1>
+            <h2>Under construction</h2>
+        </div>
+    );
+};
+
+export default Blog;
