09a26ddd65e3f50a39f2d391a4621ba75756020e
6y6a <gubinalexeymain@gmail.com>
2022-09-09 17:36:48 +0300

    feat(home): integrate projects on page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,13 +1,14 @@
 import { NextPage } from "next";
 import { Hero } from "../../features/hero";
 import styles from "./home.module.css";
-import { WorkExperience } from "../../features/content";
+import { ProjectList, WorkExperience } from "../../features/content";
 
 export const Home: NextPage = () => {
     return (
         <div className={styles.home}>
             <Hero />
             <WorkExperience />
+            <ProjectList />
         </div>
     );
 };
