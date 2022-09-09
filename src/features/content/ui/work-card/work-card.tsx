018f5dfcc11287716fe4a8201dfb88d056874c5c
6y6a <gubinalexeymain@gmail.com>
2022-09-09 13:08:03 +0300

    feat(work-card): add word card component

---
+++ src/features/content/ui/work-card/work-card.tsx
@@ -0,0 +1,31 @@
+import styles from "./work-card.module.css";
+import { FC } from "react";
+import { WorkCardProps } from "./work-card.types";
+import { format } from "date-fns";
+import { Badge } from "../../../../shared";
+
+export const WorkCard: FC<WorkCardProps> = ({
+    duration,
+    title,
+    position,
+    description,
+    link,
+}) => {
+    const start = duration[0] ? format(duration[0], "MMMM yyyy") : "Start";
+    const end = duration[1] ? format(duration[1], "MMMM yyyy") : "Present";
+
+    return (
+        <article className={styles.block}>
+            <Badge className={styles.badge}>{position}</Badge>
+
+            {link && (
+                <a href={link} target={"_blank"} className={styles.link} />
+            )}
+            <p className={styles.year}>
+                {start} - {end}
+            </p>
+            <h4 className={styles.title}>{title}</h4>
+            <p className={styles.description}>{description}</p>
+        </article>
+    );
+};
---
+++ src/features/content/ui/work-card/work-card.types.ts
@@ -0,0 +1,7 @@
+export interface WorkCardProps {
+    duration: (Date | null)[];
+    title: string;
+    position: string;
+    description: string;
+    link?: string;
+}
