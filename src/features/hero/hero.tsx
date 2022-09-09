19e7e56737669250388f6bae4e85a280d96b8bf8
6y6a <gubinalexeymain@gmail.com>
2022-09-09 09:03:36 +0300

    fix(hero): change styles

--- src/features/hero/hero.tsx
+++ src/features/hero/hero.tsx
@@ -7,61 +7,50 @@ import { HeroProps } from "./hero.types";
 export const Hero: FC<HeroProps> = () => {
     return (
         <div className={styles.block}>
-            <div className={styles.card}>
-                <div className={styles.left}>
+            <div className={styles.content}>
+                <div className={styles.avatarWrapper}>
                     <Avatar
                         src={avatar}
-                        width={200}
-                        height={200}
+                        width={120}
+                        height={120}
                         objectFit={"cover"}
                         priority={true}
                     />
                 </div>
 
-                <div className={styles.right}>
+                <div className={styles.header}>
                     <h1 className={styles.title}>Gubin Alexey</h1>
                     <h2 className={styles.position}>
                         Senior Frontend <br /> Developer
                     </h2>
                 </div>
-            </div>
 
-            <div className={styles.about}>
-                <div className={styles.left}>
-                    <div className={styles.contact}>
-                        <div>Saint-Petersburg, Russia</div>
-                        <div>mail: gubinalexeymain@gmail.com</div>
-                        <div>tg: @gubinstreet</div>
-                        <div>gh: https://github.com/6y6a</div>
-                    </div>
+                <div className={styles.contact}>
+                    <div>Saint-Petersburg, Russia</div>
+                    <div>mail: gubinalexeymain@gmail.com</div>
+                    <div>tg: @gubinstreet</div>
+                    <div>gh: https://github.com/6y6a</div>
+
+                    <h3>Languages</h3>
+                    <div>Russian (native)</div>
+                    <div>English (B1)</div>
                 </div>
-                <div className={styles.right}>
-                    <div className={styles.passion}>
-                        <p className={styles.text}>
-                            I like to build projects from proof of concepts
-                            <span className={styles.textSelection}>
-                                {" "}
-                                (PoC){" "}
-                            </span>
-                            and minimum viable products
-                            <span className={styles.textSelection}>
-                                {" "}
-                                (MVP){" "}
-                            </span>
-                            to complex systems of an enterprise-level.
-                        </p>
 
-                        <p className={styles.text}>
-                            Talk is cheap. Show me the code.
-                        </p>
+                <div className={styles.passion}>
+                    <p>
+                        I like to build projects from proof of concepts (PoC)
+                        and minimum viable products (MVP) to complex systems of
+                        an enterprise-level.
+                    </p>
 
-                        <p className={styles.text}>
-                            I have a passion for elegant, innovative and
-                            functional user interfaces. Over 5 years of
-                            experience in the frontend development, more than 3+
-                            enterprise solutions completed.
-                        </p>
-                    </div>
+                    <p>Talk is cheap. Show me the code.</p>
+
+                    <p>
+                        I have a passion for elegant, innovative and functional
+                        user interfaces. Over 5 years of experience in the
+                        frontend development, more than 3+ enterprise solutions
+                        completed.
+                    </p>
                 </div>
             </div>
         </div>
